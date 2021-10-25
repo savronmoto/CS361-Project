daf079a3d2f3ae16b2f9511c91b8c1af904bcaa7
savanna <hansonsa@oregonstate.edu>
2021-10-25 15:58:49 -0700

    Main, Login, and Adventure pages almost done

--- app.py
+++ app.py
@@ -7,38 +7,22 @@ app = Flask(__name__)
 
 # Data
 
-people_from_app_py = [
-{
-    "name": "Thomas",
-    "age": 33,
-    "location": "New Mexico",
-    "favorite_color": "Blue"
-},
-{
-    "name": "Gregory",
-    "age": 41,
-    "location": "Texas",
-    "favorite_color": "Red"
-},
-{
-    "name": "Vincent",
-    "age": 27,
-    "location": "Ohio",
-    "favorite_color": "Green"
-},
-{
-    "name": "Alexander",
-    "age": 29,
-    "location": "Florida",
-    "favorite_color": "Orange"
-}
-]
+campaigns = ["Ghosts of Saltmarsh", "Waterdeep", "Homebrew Campaign", "Curse of Strahd"]
 
 # Routes 
 
 @app.route('/')
 def root():
-    return render_template("main.j2", people=people_from_app_py)
+    return render_template("main.j2")
+
+@app.route('/login')
+def login():
+    return render_template("login.j2")
+
+@app.route('/menu')
+def menu():
+    return render_template("menu.j2", campaigns=campaigns)
+
 
 # Listener
 
