6c92fa8248075d0b06bcdff80772ee4d41d8550b
savanna <hansonsa@oregonstate.edu>
2021-10-27 13:17:23 -0700

    Dynamic Links working

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os
+from flask.helpers import url_for
+
 
 # Configuration
 
@@ -9,6 +11,17 @@ app = Flask(__name__)
 
 campaigns = ["Ghosts of Saltmarsh", "Waterdeep", "Homebrew Campaign", "Curse of Strahd"]
 
+users = [
+    {
+        "username": "bilbobaggins",
+        "password": "mithril"
+    },
+    {
+        "username": "RogueOne",
+        "password": "sneaky"
+    }
+]
+
 # Routes 
 
 @app.route('/')
@@ -17,11 +30,23 @@ def root():
 
 @app.route('/login')
 def login():
-    return render_template("login.j2")
+    return render_template("login.j2", users = users)
+
+@app.route('/menu/<campaign>', methods=['GET', 'POST'])
+def campaignView(campaign):
+    return render_template("defaultView.j2", campaign=campaign)
 
 @app.route('/menu')
 def menu():
     return render_template("menu.j2", campaigns=campaigns)
+
+    # if request.method == 'GET':
+    #     return render_template("menu.j2", campaigns=campaigns)
+    # elif request.method == 'POST':
+    #     data = request.form.get("campaigns")
+    #     return redirect(url_for('campaign', campaign=data))
+
+
 
 
 # Listener
